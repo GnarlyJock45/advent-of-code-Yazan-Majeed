2f241650f42742b9419ec45fb5f5c1d9a0f9c5c7
GnarlyJock45 <96940495+GnarlyJock45@users.noreply.github.com>
2026-03-07 00:32:09 +0300

    Add files via upload

---
+++ Ready Day 3/part1.py
@@ -0,0 +1,37 @@
+def main(): 
+    sum = 0
+    with open('textFiles/official.txt', 'r') as file:
+        for line in file:
+            row = line.strip()
+
+            # Setting the variables to reset with every new line
+            largest = 0
+            largest_index = 0
+            comb_list = []
+
+            # Using enumerate to get the index and value of each item in the list
+            for idx, n in enumerate(row):
+                n = int(n)
+
+                # Storing the maximum value over the entire list as well as its index (used for getting the largest two digits later on)
+                if n > largest:
+                    largest = n
+                    largest_index = idx
+            
+
+            # For items to the left of the largest value, they will be in the tens digit and the largest in the ones
+            for idx, n in enumerate(row):
+                if idx < largest_index:
+                    value = f"{n}{largest}"
+                    comb_list.append(int(value))
+
+                # For items to the right of the largest value, they will be in the ones digit while the largest in the tens
+                elif idx > largest_index:
+                    value = f"{largest}{n}"
+                    comb_list.append(int(value))
+
+            sum += max(comb_list)
+        print(sum)
+                
+
+main()
